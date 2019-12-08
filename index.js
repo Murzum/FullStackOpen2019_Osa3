9e88b08efc48b832c790f021d97361025d7fcf42
murzum <murzum@protonmail.com>
2019-12-08 10:33:20 +0200

    FullStackOpen2019_Osa3 - 3.2 - Valmis

--- index.js
+++ index.js
@@ -33,12 +33,38 @@ let persons = [
     app.get('/', (req, res) => {
         res.send('<h1>Hello World!</h1>')
       })
+
+    app.get('/info', (req, res) => {
+
+        const notesCount = () =>  {
+            
+            return (
+                '<h2>Phonebook has info for ' + persons.length + ' people</h2>'           
+            )
+        }
+
+        const getTimeInfo = () => {
+
+            const days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
+            const today = new Date()
+            const weekday = days[today.getDay()]
+            const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()
+            const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()
+            const datetime = date+' '+time
+
+            return ('<h2>Today is: ' + weekday + ' ' + datetime + '</h2>')
+        }
+
+        res.send( notesCount() + getTimeInfo() )
+
+    })
       
     app.get('/api/persons', (req, res) => {
-    res.json(persons)
+        res.json(persons)
     })
     
     const PORT = 3001
     app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+        console.log(`Server running on port ${PORT}`)
     })
